05fb5e4c9fc855e56731aab83e1d4b5add7e5f19
EmmaChiaNing <emma_fan@linkwish.com>
2024-06-05 12:20:09 +0800

    feat: vite 打包調整

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,23 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  build: {
+    cssCodeSplit: false,
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/js/[name]-[hash].js`,
+        chunkFileNames: `assets/js/[name]-[hash].js`,
+        assetFileNames: ({ name }) => {
+          if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')) {
+            return 'assets/image/[name]-[hash][extname]'
+          }
+          if (/\.css$/.test(name ?? '')) {
+            return 'assets/css/[name]-[hash][extname]'
+          }
+          return 'assets/[name][extname]'
+        },
+      }
+    }
+  },
 })
