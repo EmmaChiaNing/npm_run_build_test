bc0ecdde7e7a1d36d4f22a5509a79bc763c9070e
EmmaChiaNing <emma_fan@linkwish.com>
2024-06-05 12:25:08 +0800

    feat: 新增改動

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-      <h1>測試看看</h1>
+      <h1>測試看看打包</h1>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
