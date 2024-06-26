2a9981ca773e100a9090af721b3b82f2ea2665bd
EmmaChiaNing <emma_fan@linkwish.com>
2024-06-26 11:56:17 +0800

    fix: 修改 import.meta.glob 設定

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-const metaRouters = import.meta.glob("./modules/*.js");
+const metaRouters = import.meta.glob("./modules/*.js", { eager: true });
 
 const routerArray = []
 for (const key in metaRouters) {
-  const mod = await metaRouters[key]()
-  routerArray.push(...mod.default)   
+  const mod = metaRouters[key]
+  routerArray.push(...mod.default)
 }
-
+  
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
