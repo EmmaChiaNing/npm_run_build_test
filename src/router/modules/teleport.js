0dab2fa0c5707f04571d7757f436f87dd0b59744
EmmaChiaNing <emma_fan@linkwish.com>
2024-06-26 11:15:51 +0800

    fix: 修正 router 拆模組

---
+++ src/router/modules/teleport.js
@@ -0,0 +1,11 @@
+import TeleportComp from '../../views/TeleportComp.vue'
+
+const teleportView = [
+    {
+        path: '/teleport',
+        name: 'teleport',
+        component: TeleportComp
+    }
+]
+
+export default teleportView
